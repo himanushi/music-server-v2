053fc66fdb88986baafbc89a6b78dea9d90b26b7
himanushi <himanushi@users.noreply.github.com>
2021-10-25 16:15:00 +0900

    開発環境でも全て読み込む

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ require 'active_support/core_ext/integer/time'
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
