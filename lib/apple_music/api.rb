0da9ecb27d1f212a19740f55525c373509b3ae9c
himanushi <himanushi@users.noreply.github.com>
2021-11-06 06:40:37 +0900

    不要そうな機能削除

--- lib/apple_music/api.rb
+++ lib/apple_music/api.rb
@@ -38,26 +38,6 @@ module AppleMusic
       compound(body)
     end
 
-    def index(url, params = nil)
-      return {} unless @repeat.positive?
-
-      @repeat -= 1
-
-      # @type var body: ::Hash[::String, untyped]
-      body = @client.get(::AppleMusic::Api::ENDPOINT + url, params || {})
-      compound_index(body)
-    end
-
-    def index_artists_by_name(name, repeat = nil)
-      @repeat = repeat if repeat
-      # @type var params: ::Hash[::String, untyped]
-      params = {}
-      params['term']  = name
-      params['types'] = 'artists'
-      params['limit'] = '20'
-      index("#{catalog_url}/search", params)
-    end
-
     def get_artist(apple_music_id)
       get("#{catalog_url}/artists/#{apple_music_id}")
     rescue ::StandardError
@@ -104,19 +84,6 @@ module AppleMusic
         result
       else
         body
-      end
-    end
-
-    def compound_index(body)
-      if body['results']['artists']['next']
-        data = body['results']['artists']['data']
-        next_data = index(body['results']['artists']['next'])['data'] || []
-        # @type var result: ::Hash[::String, untyped]
-        result = {}
-        result['data'] = data + next_data
-        result
-      else
-        body['results']['artists']
       end
     end
 
