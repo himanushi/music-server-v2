e666ff71ab6d60bba08548c2c239b723ddc20093
himanushi <himanushi@users.noreply.github.com>
2021-11-22 18:36:28 +0900

    add AppleMusicTokenQuery

--- app/graphql/query.rb
+++ app/graphql/query.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Query < ::Types::Objects::BaseObject
-  field :album,  resolver: ::Queries::AlbumQuery
-  field :albums, resolver: ::Queries::AlbumsQuery
+  field :album,             resolver: ::Queries::AlbumQuery
+  field :albums,            resolver: ::Queries::AlbumsQuery
+  field :apple_music_token, resolver: ::Queries::AppleMusicTokenQuery
 end
