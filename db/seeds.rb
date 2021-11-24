b10ac46b96c8641e679d07d90353d434d33a4801
himanushi <himanushi@users.noreply.github.com>
2021-11-24 19:05:38 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+::ActiveRecord::Base.transaction do
+  ::Role.create!(name: 'admin', description: '管理者') do |role|
+    ::AllowedAction::ALL_ACTIONS.each do |action_name|
+      role.allowed_actions.new(name: action_name)
+    end
+  end
+
+  ::Role.create!(name: 'default', description: '未ログイン') do |role|
+    ::AllowedAction::DEFAULT_ACTIONS.each do |action_name|
+      role.allowed_actions.new(name: action_name)
+    end
+  end
+
+  ::Role.create!(name: 'login', description: 'ログイン済み') do |role|
+    ::AllowedAction::DEFAULT_ACTIONS.each do |action_name|
+      role.allowed_actions.new(name: action_name)
+    end
+  end
+
+  password = "#{::SecureRandom.hex(3)}aA1"
+
+  puts "/login [username: admin,  password: #{password}]"
+  ::User.create!(name: 'admin', username: 'admin', role: ::Role.find_by!(name: 'admin'), password: password)
+end
