47e92ea517599e4a57c423f2ae383f797727c67b
himanushi <himanushi@users.noreply.github.com>
2021-11-24 19:06:25 +0900

    cookie 焼き

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ::ActionController::Base
   before_action :current_info
+  after_action :refresh_token
 
   def current_info
     @current_info ||=
@@ -10,17 +11,36 @@ class ApplicationController < ::ActionController::Base
           session = ::Session.find_by_digit_token!(token)
           { user: session.user, session: session, cookie: request.cookies }
         rescue ::StandardError
-          user = ::User.create_user_and_session!
-          { user: user, session: user.sessions.first, cookie: request.cookies }
+          create_current_info
         end
       else
-        user = ::User.create_user_and_session!
-        { user: user, session: user.sessions.first, cookie: request.cookies }
+        create_current_info
       end
   end
+
+  def refresh_token
+    auth_setting = {
+      value: "Bearer #{current_info[:session].digit_token}",
+      max_age: ::Session::EXPIRE_DAYS.in_seconds,
+      http_only: true,
+      same_site: :lax,
+      path: '/'
+    }
+
+    auth_setting.merge!({ secure: true }) if ::Rails.env.production?
+
+    response.set_cookie('Authorization', auth_setting)
+  end
+
+  private
 
   def token
     authorization = request.cookies['Authorization']
     (authorization || '').gsub(/\ABearer /, '')
+  end
+
+  def create_current_info
+    user = ::User.create_user_and_session!
+    { user: user, session: user.sessions.first, cookie: request.cookies }
   end
 end
