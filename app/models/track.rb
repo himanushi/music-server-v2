0e6ccf64b2abed301bffccdfda732930fbe323fc
himanushi <himanushi@users.noreply.github.com>
2021-11-25 20:36:13 +0900

    add apple music tracks relation

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,6 +7,7 @@ class Track < ::ApplicationRecord
   has_many :artists, through: :artist_has_tracks
   has_many :album_has_tracks, dependent: :destroy
   has_many :albums, through: :album_has_tracks
+  has_many :apple_music_tracks, dependent: :destroy
   has_one :playlist
   has_many :playlist_items
   has_many :favorites, as: :favorable, dependent: :destroy
